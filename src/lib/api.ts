ad674be7be43f298bffe7c85a5c137f3a9a4b055
paavann <pavann22826@gmail.com>
2025-04-26 11:39:04 +0530

    initialized the project. built interceptor

---
+++ src/lib/api.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL
+})
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("access")
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (err) => {
+        return Promise.reject(err)
+    }
+)
+
+
+export default api
