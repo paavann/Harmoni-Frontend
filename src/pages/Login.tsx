4c13c1a9ce16df2d97200c0ad12cb08197502f80
paavann <pavann22826@gmail.com>
2025-04-30 17:10:16 +0530

    installed shadcn. changed default font to montserrat. started with implementing login page.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,31 @@
+import React from 'react'
+import { LoginForm } from '@/components/login-form'
+import { useNavigate } from 'react-router-dom'
+
+import api from '@/lib/api'
+
+
 function Login() {
+    const navigate = useNavigate()
+
+    const handleLogin = async (e: React.FormEvent, username: string, password: string) => {
+        e.preventDefault()
+
+        try {
+            const res = await api.post('http://localhost:8000/users/token', { username, password })
+            localStorage.setItem("access", res.data.access)
+            localStorage.setItem("refresh", res.data.refresh)
+            navigate('/')
+        } catch (err) {
+            alert(err)
+        }
+    }
+
     return (
-        <div className="flex justify-center bg-blue-500">
-            <h1>Login to Harmoni</h1>
+        <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+            <div className="w-full max-w-sm md:max-w-3xl">
+                <LoginForm />
+            </div>
         </div>
     )
 }
