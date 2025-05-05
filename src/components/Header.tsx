91bda153bc0b5a90848a0322b2edf12b570881f0
paavann <pavann22826@gmail.com>
2025-05-05 14:10:40 +0530

    built a basic header layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import { PanelLeftOpen } from 'lucide-react'
+import { BookPlus } from 'lucide-react'
+
+
+
+function Header() {
+    const currDate = new Date()
+    const formattedDate = `${String(currDate.getDate()).padStart(2, '0')} - ${String(currDate.toLocaleString('default', { month: 'short' }))} - ${currDate.getFullYear()}`
+
+    return (
+        <div className="w-full h-full bg-[#905fd4] p-2 flex justify-between items-center">
+            <div className='w-[33.4%]'>
+                <PanelLeftOpen
+                    className="hover:cursor-pointer"
+                    color='#ffffff'
+                    strokeWidth={1}
+                />
+            </div>
+            <div className="text-white font-semibold w-[33.4%] justify-center flex">
+                Title, {formattedDate}
+            </div>
+            <div className="w-[33.4%] flex justify-end">
+                <BookPlus
+                    className="hover:cursor-pointer"
+                    color='#ffffff'
+                    strokeWidth={1}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
+import Header from "@/components/Header"
+
 function Home() {
     return (
-        <div>
+        <div className="w-[100vw] h-[100vh]">
+            <div className="w-full h-[5%]">
+                <Header />
+            </div>
             <h1>your home page</h1>
         </div>
     )
